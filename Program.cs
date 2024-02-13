5c0624513ee8cd3142042971b7a384ada2ff7b70
Admin <Admin@CSLAB221>
2024-02-13 19:30:31 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Quiz1;
+using System;
+
+namespace Quiz1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+
+    }
+}
+
+class Progress
+{
+    static void Main(string[] args)
+    {
+        List<Person> list = new List<Person>();
+
+        while (true)
+        {
+            Console.WriteLine("Enter new data? [Y/N]");
+
+            if (Console.ReadLine() == "N")
+            {
+                break;
+            }
+
+            Person person1 = new Person();
+
+            Console.WriteLine("Enter Person Name = ");
+            person1.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter Person Weight = ");
+            person1.Weight = int.Parse(Console.ReadLine());
+
+            list.Add(person1);
+        }
+
+        list.ForEach(x =>
+        {
+            Console.WriteLine(x.Name);
+            Console.WriteLine(x.Weight);
+            Console.WriteLine("=====================");
+        });
+
+    }
+}
